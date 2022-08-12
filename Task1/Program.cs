3acae3c0dd67fa36161b301025856449470fea9d
maximandreev <maxands@yandex.ru>
2022-08-12 17:05:12 +0300

    Task 1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] digits = new int[5];
+int i = 1;
+while (i != 6)
+{
+    digits[i-1] = number % 10;
+    number /= 10;
+    i++;
+}
+if (digits[0] == digits[4] && digits[1] == digits[3])
+{
+    Console.WriteLine("Ваше число палиндром");
+}
+else
+{
+    Console.WriteLine("Ваше число не палиндром");
+}
