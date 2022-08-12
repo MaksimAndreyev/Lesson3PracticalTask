5944d986841cad909b83eaf291b50f3f403f4b12
maximandreev <maxands@yandex.ru>
2022-08-12 17:16:03 +0300

    Task 3 completed

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while(i <= N)
+{
+    if (i == N)
+    {
+        Console.WriteLine(i*i*i);
+        break;
+    }
+    Console.Write(i*i*i + ", ");
+    i++;
+}
